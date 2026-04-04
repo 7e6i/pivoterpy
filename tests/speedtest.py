e315a26487d5c373f3936838e9806dfbbecc8cc4
Spencer <the7e6i@gmail.com>
2026-04-04 16:44:57 -0400

    refactored benchmark function

---
+++ tests/speedtest.py
@@ -0,0 +1,43 @@
+
+from time import time
+from random import random
+from pivoterpy import Pivoter
+
+def complete_graph(n):
+    return [[1 for _ in range(n)] for _ in range(n)]
+
+def erodos_reyni(n, p):
+    return [[random() < p for _ in range(n)] for _ in range(n)]
+
+
+    
+def timer():
+
+    inits = []
+    times = []
+
+    for _ in range(10):
+
+        #G = complete_graph(200)
+        G = erodos_reyni(1000, .1)
+
+        t0 = time()
+        H = Pivoter.from_adj_matrix(G)
+        t = time() - t0
+        inits.append(t)
+        print(f'{t:.4f}s,', end=' ')
+
+        t0 = time()
+        H.count(get_curv=True)
+        t = time() - t0
+        times.append(t)
+        print(f'{_+1}: {t:.4f}s')
+
+
+    print(f'Inits {sum(inits)/len(inits):.2f}s')
+    print(f'Counts {sum(times)/len(times):.2f}s')
+    print(H.ec, H.max_k)
+    print(H.clique_counts)
+
+
+timer()
