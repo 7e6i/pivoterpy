a1a88f2bfbc2209a783989fcdd8de9415011c726
Spencer <the7e6i@gmail.com>
2026-04-04 16:44:41 -0400

    gnp graph test outline

--- tests/test_pivoter.py
+++ tests/test_pivoter.py
@@ -1,7 +1,6 @@
 # tests/test_pivoter.py
 from pivoterpy import Pivoter
 
-from time import time
 from random import random
 from math import comb
 
@@ -10,34 +9,6 @@ def complete_graph(n):
 
 def erodos_reyni(n, p):
     return [[random() < p for _ in range(n)] for _ in range(n)]
-    
-def timer():
-
-    inits = []
-    times = []
-
-    for _ in range(10):
-
-        #G = complete_graph(200)
-        G = erodos_reyni(1000, .1)
-
-        t0 = time()
-        H = Pivoter.from_adj_matrix(G)
-        t = time() - t0
-        inits.append(t)
-        print(f'{t:.4f}s,', end=' ')
-
-        t0 = time()
-        H.count(get_curv=True)
-        t = time() - t0
-        times.append(t)
-        print(f'{_+1}: {t:.4f}s')
-
-
-    print(f'Inits {sum(inits)/len(inits):.2f}s')
-    print(f'Counts {sum(times)/len(times):.2f}s')
-    print(H.ec, H.max_k)
-    print(H.clique_counts)
 
 
 def test_complete_graph():
@@ -61,7 +32,25 @@ def test_complete_graph():
     tol = 1e-6
     assert abs(H.ec - sum(H.curvatures)) < tol and abs(H.ec - sum(H.vertex_curv)) < tol
 
+
+def test_gnp():
+    n, p = 10, 0.1
+    G = erodos_reyni(n, p)
+
+    H = Pivoter.from_adj_matrix(G)
+    assert H.n == n
+    
+    H.count(get_curv=True)
+    assert H.ec == H.global_ec
+
+    # TODO idk, K-S test for binomial distribution?
+
+    tol = 1e-6
+    assert abs(H.ec - sum(H.curvatures)) < tol and abs(H.ec - sum(H.vertex_curv)) < tol
+
+
+
 if __name__ == '__main__':
     test_complete_graph()
 
-    timer()
+    test_gnp()
