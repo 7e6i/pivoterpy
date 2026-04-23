b80e609349a3e59fd25945f809dfc1f957cd9c55
Spencer <spencera.rochester@gmail.com>
2026-04-23 18:42:51 -0400

    updated rust build

--- build.rs
+++ build.rs
@@ -1,4 +1,11 @@
+//! Build script for the experimental CUDA backend.
+//!
+//! This script uses the `cc` crate to compile the `.cu` files using `nvcc`
+//! and instructs Cargo to link the resulting library along with the CUDA runtime.
+
 // build.rs
+
+/// Compiles `pivoter.cu` and configures the linker for CUDA and C++ standard libraries.
 fn main() {
     println!("cargo:rerun-if-changed=src/cuda/pivoter.cu");
 
