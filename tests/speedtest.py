83539db36f5ba76e996362f691a7feada5aa66e8
Spencer <spencera.rochester@gmail.com>
2026-04-27 19:39:43 -0400

    run out of memory with com-orkut

--- tests/speedtest.py
+++ tests/speedtest.py
@@ -5,39 +5,48 @@ import pivoterpy as pvt
 
 
 def read_file(filename):
-    edges = []
-    with open(filename) as f:
-        lines = f.readlines()
-
-    for line in lines:
-        line = line.strip()
-        if line.startswith('#'):
-            continue
-        else:
-            edges.append(tuple(map(int, line.split())))
-    return edges
-
+    with open(filename, 'r') as f:
+        # 1. Iterating directly over `f` avoids the memory spike of readlines()
+        # 2. List comprehension avoids .append() overhead
+        # 3. .split() automatically ignores trailing newlines/whitespace
+        return [
+            tuple(map(int, line.split()))
+            for line in f 
+            if not line.startswith('#')
+        ]
 
 def main():
-    files = ['com-dblp', 'as-skitter']
-    edges = read_file(f'tests/{files[1]}.edges')
+    files = ['com-dblp', 'as-skitter', 'com-lj', 'com-orkut']
+    # cant do com-orkut
 
+    t0 = time()
+    edges = read_file(f'tests/{files[2]}.edges')
+    print(f"disk: {time() - t0:.3f}")
 
     t0 = time()
     G = pvt.from_edge_list(edges)
-    print(f"loaded: {time() - t0:.3f}")
+    print(f"graph: {time() - t0:.3f}")
 
-    t0 = time()
-    P = pvt.pivoter(G, procs=8, backend='rust', resolution='g', min_k=11,max_k=11)
-    print(f'{time() - t0:.3f} seconds')
 
-    #print(P.global_counts)
+    for k in range(6,1000000):
+        t0 = time()
+        P = pvt.pivoter(G, procs=8, min_k=k, max_k=k)
+        tc = time() - t0
+        num = P.global_counts[-1]
+        if not num:
+            break
+        print(f'{k},{round(tc,3):.3f},{num}')
 
 
 
 if __name__ == "__main__":
     main()
 
-# 8 threads - global, vertex, edge (seconds)
-# com-dblp - 0.494, 3.426, 19.925
-# as-skitter - 33, 137, no
+
+# disk: 52.938
+# graph: 152.907
+# 0,0.036,1
+# 1,0.035,3072441
+# 2,0.035,117185083
+# 
+# 
